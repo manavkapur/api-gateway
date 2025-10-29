8f2ac46e725fe8efedd81cff4f7bb9c7ba13b986
Manav Kapur <62932155+manavkapur@users.noreply.github.com>
2025-10-30 05:17:16 +0530

    Cors fixed

---
+++ src/main/java/com/supremesolutions/api_gateway/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.supremesolutions.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://supremebuildsolutions.vercel.app", // ✅ Frontend
+                                "http://localhost:3000" // ✅ Local Dev
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
